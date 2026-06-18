fa40438818c328582b46e792815ea57f8c95b437
Argobel <51906090+Argobelz@users.noreply.github.com>
2026-06-18 18:39:19 +0700

    chore: bump APP_VERSION to 0.6

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ except Exception:
 # ---------------------------------------------------------------------------
 
 APP_TITLE     = "Aeternus Renderer"
-APP_VERSION   = "0.5"
+APP_VERSION   = "0.6"
 ADDON_VERSION = "2.3"
 APP_PORT      = 47821
 CONFIG_DIR    = os.path.join(os.path.expanduser("~"), ".aeternus_renderer")
